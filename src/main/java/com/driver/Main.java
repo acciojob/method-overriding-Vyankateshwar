95da3b78492af230432a127c85c40ce5136fd62f
Sangmeshwar19 <ganub964@gmail.com>
2023-04-07 00:34:50 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        B obj = new B ();
+        System.out.println(obj.meth());
+
+    }
+
+
   
 }
